63c649a1795123cae1c5e4d509f624a30eaaee71
Aikawa <cesar.lojan8@gmail.com>
2023-04-12 15:55:55 +0000

    task

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,7 @@
+import { Container, Heading, SimpleGrid } from '@chakra-ui/react'
+import Column from './components/Column';
+import { ColumnType } from './utils/enums';
 
-import { Container, Heading } from '@chakra-ui/react'
 function App() {
 
   return (
@@ -15,7 +17,13 @@ function App() {
         DnD kanban
       </Heading>
       <Container maxWidth="container.lg" px={4} pt={10}>
-
+        <SimpleGrid columns={{base:1,md:4}} spacing={{base:16,md:4}}>
+          <Column column={ColumnType.TO_DO}/>
+          <Column column={ColumnType.IN_PROGRESS}/>
+          <Column column={ColumnType.BLOCKED}/>
+          <Column column={ColumnType.COMPLETED}/>
+          
+        </SimpleGrid>
       </Container>
       </>
   )
